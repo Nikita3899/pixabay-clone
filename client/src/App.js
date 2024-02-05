6b9c600d6659d122906a62096cc82b3c34a2b485
inkzup <inkzup6@gmail.com>
2024-02-05 21:28:14 +0530

    added tailwind

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+
+
+function App() {
+  return (
+    <div className="w-screen h-screen">
+     <h1 className="text-2xl text-blue-400">App</h1>
+     {/* header */}
+     {/* main-tab-section */}
+    </div>
+  );
+}
+
+export default App;
